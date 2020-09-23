eeb7ebe2089bd3ea6b379226b67af9478cc911e4
Ian Magenta <ianmagenta@gmail.com>
2020-09-22 21:38:19 -0700

    updated mouse script

--- scenes/screen/Mouse.gd
+++ scenes/screen/Mouse.gd
@@ -4,4 +4,5 @@ func _init():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _input(event):
-	position = get_global_mouse_position()
+	if event is InputEventMouseMotion:
+		position = get_global_mouse_position()
