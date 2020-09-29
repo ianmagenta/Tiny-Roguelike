ffb599a5e3974ce68a7cfad84ee84a84d48b8534
Ian Magenta <ianmagenta@gmail.com>
2020-09-29 12:57:53 -0700

    reorganized game scene around canvaslayers.

--- scripts/resources/text_effects/Typewriter.gd
+++ scripts/resources/text_effects/Typewriter.gd
@@ -6,9 +6,10 @@ class_name Typewriter
 var bbcode = "type"
 
 func _process_custom_fx(char_fx):
-	var wait_time: float = char_fx.env.get("wait", 0.0)
-	var speed: float = char_fx.env.get("speed", 32.0)
 	var elapsed_time = char_fx.elapsed_time
+	if elapsed_time > 5.0: return false
+	var wait_time: float = char_fx.env.get("wait", 0.0)
+	var speed: float = char_fx.env.get("speed", 64.0)
 	if elapsed_time > wait_time and float(elapsed_time) > ((float(char_fx.relative_index) / speed) + float(wait_time)):
 		char_fx.visible = true
 	else:
