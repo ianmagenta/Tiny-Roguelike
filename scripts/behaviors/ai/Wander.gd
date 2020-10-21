d7fb896b19867175c7c51f260af3f54b0a5f9ccf
Ian Magenta <ianmagenta@gmail.com>
2020-10-20 17:23:54 -0700

    removed test message log call from bat script.

--- scripts/behaviors/ai/Wander.gd
+++ scripts/behaviors/ai/Wander.gd
@@ -1,3 +1,2 @@
 func execute(command: PreTurn):
-	Globals.message_log.add_message("Boogie woogie! This little batty watty likes to wiggle diggle its litte dittle butt as it flies! " + command.receiver.get_name())
 	return MoveRandomly.new(command.invoker, command.receiver)
